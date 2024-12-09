409bead3cb4da233939b439744b37570c260b47c
Dvermetten <d.vermetten@gmail.com>
2024-12-09 14:02:05 +0100

    Add attractor network visualization

--- src/iohinspector/metrics.py
+++ src/iohinspector/metrics.py
@@ -398,3 +398,99 @@ def add_normalized_objectives(data: pl.DataFrame, obj_cols: Iterable[str], max_v
         return data.with_columns([(data[colname]/data[colname].max()).alias(f'obj{idx + 1}') for idx, colname in enumerate(obj_cols)])
 
 
+def _get_nodeidx(xloc, yval, nodes, epsilon):
+    if len(nodes) == 0:
+        return -1
+    candidates = nodes[np.isclose(nodes['y'], yval, atol = epsilon)]
+    if len(candidates) == 0:
+        return -1
+    idxs = np.all(np.isclose(np.array(candidates)[:, :len(xloc)], xloc, atol=epsilon), axis=1)
+    if any(idxs):
+        return candidates[idxs].index[0]
+    return -1
+
+def get_attractor_network(data, 
+                           coord_vars = ['x1', 'x2'],
+                           fval_var: str = 'raw_y',
+                           eval_var: str = 'evaluations',
+                           maximization: bool = False,
+                           beta=40, 
+                           epsilon=0.0001,
+                           eval_max = None):
+    """Create an attractor network from the provided data
+
+    Args:
+        data (pl.DataFrame): The original dataframe, should contain the performance and position information
+        coord_vars (Iterable[str], optional): Which columns correspond to position information. Defaults to ['x1', 'x2'].
+        fval_var (str, optional): Which column corresponds to performance. Defaults to 'raw_y'.
+        eval_var (str, optional): Which column corresponds to evaluations. Defaults to 'evaluations'.
+        maximization (bool, optional): Whether fval_var is to be maximized. Defaults to False.
+        beta (int, optional): Minimum stagnation lenght. Defaults to 40.
+        epsilon (float, optional): Radius below which positions should be considered identical in the network. Defaults to 0.0001.
+        eval_max (int, optional): Maximum evaluation number. Defaults to the maximum of eval_var if None.
+    Returns:
+        pd.DataFrame, pd.DataFrame: two dataframes containing the nodes and edges of the network respectively.
+    """
+        
+    running_idx = 0
+    running_edgeidx = 0
+    nodes = pd.DataFrame(columns=[*coord_vars, 'y', 'count', 'evals'])
+    edges = pd.DataFrame(columns=['start', 'end', 'count', 'stag_length_avg'])
+    if eval_max is None:
+        eval_max = max(data[eval_var])
+
+    for run_id in data['data_id'].unique():
+        dt_group = data.filter(pl.col('data_id') == run_id, pl.col(eval_var) <= eval_max)
+        if maximization:
+            ys = np.maximum.accumulate(np.array(dt_group[fval_var]))
+        else:
+            ys = np.minimum.accumulate(np.array(dt_group[fval_var]))
+        xs = np.array(dt_group[coord_vars])
+
+        stopping_points = np.where(np.abs(np.diff(ys, prepend=np.inf))>0)[0]
+        evals = np.array(dt_group[eval_var])
+        
+        stagnation_lengths = np.diff(evals[stopping_points], append=eval_max)
+        edge_lengths = stagnation_lengths[stagnation_lengths>beta]
+        real_idxs = [stopping_points[i] for i in np.where(stagnation_lengths>beta)[0]]
+
+        xloc = xs[real_idxs[0]]
+        yval = ys[real_idxs[0]]
+        nodeidx = _get_nodeidx(xloc, yval, nodes, epsilon)
+        if nodeidx == -1:
+            nodes.loc[running_idx] = [*xloc, yval, 1, evals[real_idxs[0]]]
+            node1 = running_idx
+            running_idx += 1
+        else:
+            nodes.loc[nodeidx, 'evals'] += evals[real_idxs[0]]
+            nodes.loc[nodeidx, 'count'] += 1
+            node1 = nodeidx
+        
+        if len(real_idxs) == 1:
+            continue
+
+        for i in range(len(real_idxs)-1):
+            xloc = xs[real_idxs[i+1]]
+            yval = ys[real_idxs[i+1]]
+            nodeidx = _get_nodeidx(xloc, yval, nodes, epsilon)
+            if nodeidx == -1:
+                nodes.loc[running_idx] = [*xloc, yval, 1, evals[real_idxs[i+1]]]
+                node2 = running_idx
+                running_idx += 1
+            else:
+                nodes.loc[nodeidx, 'evals'] += evals[real_idxs[i+1]]
+                nodes.loc[nodeidx, 'count'] += 1
+                node2 = nodeidx
+
+            edgelen = edge_lengths[i]
+            edge_idxs = edges.query(f"start == {node1} & end == {node2}").index
+            if len(edge_idxs) == 0:
+                edges.loc[running_edgeidx] = [node1, node2, 1, edgelen]
+                running_edgeidx += 1 
+            else:
+                curr_count = edges.loc[edge_idxs[0]]['count']
+                curr_len = edges.loc[edge_idxs[0]]['stag_length_avg']
+                edges.loc[edge_idxs[0], 'stag_length_avg'] = (curr_len*curr_count + edgelen)/(curr_count+1)
+                edges.loc[edge_idxs[0], 'count'] += 1
+            node1 = node2
+    return nodes, edges
--- src/iohinspector/plot.py
+++ src/iohinspector/plot.py
@@ -16,7 +16,8 @@ from .metrics import (
     transform_fval,
     get_sequence,
     aggegate_convergence,
-    get_glicko2_ratings
+    get_glicko2_ratings,
+    get_attractor_network
 )
 from .align import align_data
 from .indicators import add_indicator, Final_NonDominated
@@ -752,3 +753,93 @@ def plot_robustrank_changes(
     if filename_fig:
         plt.savefig(filename_fig)
         plt.close()
+
+
+def plot_attractor_network(
+                        data, 
+                        coord_vars: Iterable[str] = ['x1', 'x2'],
+                        fval_var: str = 'raw_y',
+                        eval_var: str = 'evaluations',
+                        maximization: bool = False,
+                        beta=40, 
+                        epsilon=0.0001
+):
+    """Plot an attractor network from the provided data
+
+    Args:
+        data (pl.DataFrame): The original dataframe, should contain the performance and position information
+        coord_vars (Iterable[str], optional): Which columns correspond to position information. Defaults to ['x1', 'x2'].
+        fval_var (str, optional): Which column corresponds to performance. Defaults to 'raw_y'.
+        eval_var (str, optional): Which column corresponds to evaluations. Defaults to 'evaluations'.
+        maximization (bool, optional): Whether fval_var is to be maximized. Defaults to False.
+        beta (int, optional): Minimum stagnation lenght. Defaults to 40.
+        epsilon (float, optional): Radius below which positions should be considered identical in the network. Defaults to 0.0001.
+
+    Returns:
+        pd.DataFrame, pd.DataFrame: two dataframes containing the nodes and edges of the network respectively.
+    """
+    try:
+        import networkx as nx
+    except:    
+        print("NetworkX is required to use this plot type")
+        return
+    from sklearn.decomposition import MDS
+
+    nodes, edges = get_attractor_network(data, 
+                                          maximization,
+                                          coord_vars,
+                                          fval_var, 
+                                          eval_var,
+                                          beta, 
+                                          epsilon)
+    network = nx.DiGraph()
+    for idx, row in nodes.iterrows():
+        network.add_node(idx, decision=np.array(row)[:len(coord_vars)], fitness=row['y'],
+                                hitcount=row['count'], evals=row['evals']/row['count'])
+
+    for _, row in edges.iterrows():
+        network.add_edge(row['start'], row['end'], weight=row['count'], evaldiff=row['stag_length_avg'])
+    network.remove_edges_from(nx.selfloop_edges(network))
+
+    decision_matrix = [network.nodes[node]['decision'] for node in network.nodes()]
+    mds = MDS(n_components=1, random_state=0)
+    x_positions = mds.fit_transform(decision_matrix).flatten()  # Flatten to get 1D array for x-axis
+    y_positions = [network.nodes[node]['fitness'] for node in network.nodes()]
+    pos = {node: (x, y) for node, x, y in zip(network.nodes(), x_positions, y_positions)}
+
+    hitcounts = [network.nodes[node]['hitcount'] for node in network.nodes()]
+    if len(hitcounts) > 1:
+        min_hitcount = min(hitcounts)
+        max_hitcount = max(hitcounts)
+    # Node sizes and colors based on fitness values (as in your original code)
+    if len(hitcounts) > 1 and np.std(hitcounts) > 0:
+        node_sizes = [
+        100 + (400 * (network.nodes[node]['hitcount'] - min_hitcount) / (max_hitcount - min_hitcount))
+        for node in network.nodes()
+        ]
+    else:
+        node_sizes = [500]*len(hitcounts)
+    fitness_values = y_positions  # Reuse y_positions as they represent 'fitness'
+    norm = plt.Normalize(min(fitness_values), max(fitness_values))
+    node_colors = plt.cm.viridis(norm(fitness_values))
+
+    # Draw the graph
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 6))
+    nx.draw(
+        network,
+        pos=pos,
+        with_labels=False,
+        node_size=node_sizes,
+        node_color=node_colors[:, :3],
+        edge_color='gray',
+        width=2,
+        ax=ax
+    )
+
+    # Add colorbar for fitness values
+    sm = plt.cm.ScalarMappable(cmap='viridis', norm=norm)
+    sm.set_array(fitness_values)
+    plt.xlabel("MDS-reduced decision vector")
+    plt.ylabel("fitness")
+    plt.tight_layout()
