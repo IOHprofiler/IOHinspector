edf7eb7384fe52730238182c4cf6067a4783c2a5
Dinu23 <viorel.dinu00@gmail.com>
2025-09-26 15:36:08 +0200

    shorter test

--- tests/test_data.py
+++ tests/test_data.py
@@ -220,7 +220,8 @@ class TestManager(unittest.TestCase):
         self.assertIn("algorithm_info", df.columns)
         self.assertEqual(df["algorithm_name"].unique().to_list(), ["BFGS-scipy-2019"])
         self.assertEqual(df["suite"].unique().to_list(), ["bbob"])
-        self.assertEqual(df["function_id"].unique().to_list(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
+        self.assertEqual(df["function_id"].unique().to_list(), [1])
+        self.assertEqual(df["dimension"].unique().to_list(), [2])
 
 if __name__ == "__main__":
     unittest.main()
